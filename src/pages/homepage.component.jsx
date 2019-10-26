d1098f48d25f9acd8a6bdfe9b37e270deb3f8c78
shamimjozi <jozishamim@gamil.com>
2019-10-26 20:19:03 +0330

    broken our homepage into smaller components, finished styling our menu-item component

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import './homepage.styles.scss';
+
+import Directory from '../components/directory/directory.component';
+
+//we use function because we don't need any cycle methodts or don't want to store s.th;
+
+ const HomePage = () => (
+    <div className='homepage'>
+       <Directory />
+    </div>
+)
+
+export default HomePage ;
