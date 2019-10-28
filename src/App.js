35bf47fd9b706521192a5593f7492a05762fa6b1
shamimjozi <jozishamim@gamil.com>
2019-10-28 12:58:00 +0330

    updating our app with a shop page , collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import React from 'react';
 import './App.css';
 import {Switch , Route} from 'react-router-dom' ;
 
-import HomePage from './pages/homepage.component' ;
+import HomePage from './pages/homepage/homepage.component' ;
+import ShopPage from './pages/shop/shop.component';
 import { from } from 'rxjs';
 
-const HatsPage = () =>(
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+
 
 function App() {
   return (
     <div >
       <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/shop/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
       </Switch>
        
      </div>
---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react' ;
+
+import SHOP_DATA from './shop.data.js' ;
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import { ssh } from 'node-forge';
+
+class ShopPage extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state ={
+            collection : SHOP_DATA 
+        }
+    }
+
+    render(){
+        const {collection} = this.state ;
+        return(
+                <div className='shop-page'> 
+                   {
+                       collection.map(({id , ...otherCollectionProps}) => (
+                           <CollectionPreview key={id} {...otherCollectionProps} />
+                       ))
+                   } 
+                </div>
+        );
+    }
+}
+
+export default ShopPage ;
+
+//...otherCollection is other part of the state for passing to the component
