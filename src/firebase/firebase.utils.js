2677b1ccdedb8b71371fbdfb963b51859ee171cc
shamimjozi <jozishamim@gamil.com>
2019-11-07 00:00:49 +0330

    implementedfirebase utils, included ability to store authenticated user data base

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,33 @@ const config = {
     measurementId: "G-XSGCQ2EYQV"
 }
 
+//take the user in authentication library and store in data base
+
+export const createUserProfileDocument = async (userAuth , additionalData ) =>{
+    if(!userAuth) return ;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+    
+    if (!snapShot.exists){
+        const {displayName , email} = userAuth ;
+        const createdAt = new Date();
+
+        try{
+            await userRef.set({
+                displayName ,
+                email,
+                createdAt ,
+                ...additionalData
+            });
+        } catch(error){
+            console.log('error creating user' , error.message);
+        }
+    }
+
+    return userRef ;
+}
+
 firebase.initializeApp(config) ;
 
 export const auth = firebase.auth();
--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
 import './signin-signup.styles.scss';
 
 const SignInAndSignUpPage = ()=>(
     <div className='signin-signup'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
